be76e173e58a61496a3114cc9814037687884341
Billy <kwong.billy@gmail.com>
2016-07-25 21:58:11 +0000

    First commit - setting up project

---
+++ server.js
@@ -0,0 +1,18 @@
+var unirest = require('unirest');
+var express = require('express');
+var events = require('events');
+
+var getFromApi = function(endpoint, args) {
+    var emitter = new events.EventEmitter();
+    unirest.get('https://api.spotify.com/v1/' + endpoint)
+           .qs(args)
+           .end(function(response) {
+                if (response.ok) {
+                    emitter.emit('end', response.body);
+                }
+                else {
+                    emitter.emit('error', response.code);
+                }
+            });
+    return emitter;
+};
