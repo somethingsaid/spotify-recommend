1bd8f809a4afa501e67b8a9b95cc9e93e4ca74f8
Billy <kwong.billy@gmail.com>
2016-07-25 22:04:04 +0000

    Added spotify API and endpoints

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var unirest = require('unirest');
 var express = require('express');
 var events = require('events');
+var app = express();
+app.use(express.static('public'));
 
+// Function to call Spotify API
 var getFromApi = function(endpoint, args) {
     var emitter = new events.EventEmitter();
     unirest.get('https://api.spotify.com/v1/' + endpoint)
@@ -16,3 +19,23 @@ var getFromApi = function(endpoint, args) {
             });
     return emitter;
 };
+
+// Creating endpoints with express
+app.get('/search/:name', function(req, res) {
+    var searchReq = getFromApi('search', {
+        q: req.params.name,
+        limit: 1,
+        type: 'artist'
+    });
+
+    searchReq.on('end', function(item) {
+        var artist = item.artists.items[0];
+        res.json(artist);
+    });
+
+    searchReq.on('error', function(code) {
+        res.sendStatus(code);
+    });
+});
+
+app.listen(8080);
